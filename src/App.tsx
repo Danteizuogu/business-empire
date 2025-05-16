5cf39fb692b9be34bff9b337df3204a29176b175
danteizuogu <dante.izuogu@proton.me>
2025-05-16 13:39:53 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <h1 className="title">Business Empire</h1>
+        <div className="button-group">
+          <button className="premium-button play">Play</button>
+          <button className="premium-button how-to">How to Play</button>
+          <button className="premium-button leaderboard">Leaderboard</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
